35e2c5c6b377dcea9af70295261ae7efc1211307
HenriqueHiideki <henrique.hiideki@gmail.com>
2026-07-27 18:05:48 -0300

    Entrada verifica espacos em branco e impede de adiciona-los

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 function adicionarTarefa() {
   //adicona mensagem de sucesso toda vez que uma tarefa for inserida.
-  let mensagem = "Tarefa adicionada com sucesso!"
   let inputTarefa = document.getElementById("inputTarefa");
-  let tarefa = inputTarefa.value;
-  document.getElementById("mensagem").textContent = mensagem;
+  let tarefa = inputTarefa.value.trim();
 
-  //cria o elemento li de uma lista das tarefas adicionadas.
-  let listaTarefas = document.getElementById("listaTarefas");
-  let novaTarefa = document.createElement("li");
+  if (tarefa == "") {
+    let mensagemErro = "Digite uma tarefa para adiciona-la a sua lista!";
+    document.getElementById("mensagem").textContent = mensagemErro;
+  } else {
+    let mensagem = "Tarefa adicionada com sucesso!";
+    document.getElementById("mensagem").textContent = mensagem;
 
-  //adiciona a tarefa como um novo item sobre um elemento pai.
-  novaTarefa.textContent = tarefa;
-  listaTarefas.appendChild(novaTarefa);
+    //cria o elemento li de uma lista das tarefas adicionadas.
+    let listaTarefas = document.getElementById("listaTarefas");
+    let novaTarefa = document.createElement("li");
+
+    //adiciona a tarefa como um novo item sobre um elemento pai.
+    novaTarefa.textContent = tarefa;
+    listaTarefas.appendChild(novaTarefa);
+  }
 
   //limpa a area de entrada.
   inputTarefa.value = "";
